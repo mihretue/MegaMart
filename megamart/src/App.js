0eb3c6f5d642e9325234c63c2b3edda5610e279d
Mihretu Endeshaw <106491338+mihretue@users.noreply.github.com>
2024-07-28 18:13:04 +0300

    adding logo and adjusting common pages

--- megamart/src/App.js
+++ megamart/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductPage from "./page/productPage";
 import NavBar from "./component/NavBar";
+import HomePage from "./page/HomePage";
+import AboutPage from "./page/AboutPage";
+import Contact from "./page/Contact";
+
+
 function App() {
   return (
     <BrowserRouter>
@@ -8,7 +13,10 @@ function App() {
         <NavBar />
         <main className="container mt-4">
         <Routes>
-          <Route path="/" element={<ProductPage/>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product" element={<ProductPage/>} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<Contact/>}/>
         </Routes>
         </main>
       </div>
--- megamart/src/component/NavBar.jsx
+++ megamart/src/component/NavBar.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import MegaMartBillboard7 from '../image/MegaMartBillboard7.png'
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">MyApp</a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mx-3">
+      <Link className="navbar-brand rounded " to="#">
+        <img src={MegaMartBillboard7} alt="Logo" className='rounded-circle' style={{width:'8rem', height:'7rem'}}/>
+      </Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">Home</Link>
+      <div className="collapse navbar-collapse " id="navbarNav">
+        <ul className="navbar-nav fw-bolder  ">
+          <li className="nav-item ">
+            <Link className="nav-link text-primary" to="/">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/about">About</Link>
+            <Link className="nav-link text-primary" to="/product">Product</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" to="/contact">Contact</Link>
+            <Link className="nav-link text-primary" to="/about">About</Link>
+          </li>
+          <li>
+            <Link className="nav-link text-primary" to="/contact">Contact</Link>
           </li>
         </ul>
       </div>
